5a05714f009663371b87df2e863861f46e6494be
Seamus <bl4ster9000@gmail.com>
2026-04-15 19:09:52 -0400

    started progamming the code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-export default function Hello() {
-  return <p>Hello, world</p>;
+import styles from styles.css;
+const headerNames = {
+  aboutMe: "About Me",
+  Skills: "Skills",
+  Projects: "Projects"
+};
+const contactDetails = {
+  email: "sd30149p@pace.edu",
+  linkedIn: "www.linkedin.com/in/seamus-doherty-1a84173b0",
+  gitHub: "https://github.com/SeamusDoherty1"
+};
+
+export default function Headers() {
+  return (
+    <>
+    <h1>{headerNames.aboutMe}, {headerNames.Skills}, {headerNames.Projects}</h1>
+    </>
+  );
+
 }
 
